7e58868eaaecd9fddaa40d6ea01f48c2f6bcc9ac
Ryan Pastoriza <ryan.pastoriza@dataelites.io>
2026-05-22 10:40:55 +0800

    Revert "fix(nexus): SessionReset broadcasts synchronously like other order events"
    This reverts commit 655bda605266bb95684eb5c8bc41f96fbf130633.

--- app/Events/SessionReset.php
+++ app/Events/SessionReset.php
@@ -6,11 +6,11 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Foundation\Events\Dispatchable;
 
-class SessionReset implements ShouldBroadcastNow
+class SessionReset implements ShouldBroadcast
 {
     use InteractsWithSockets, SerializesModels, Dispatchable;
 
